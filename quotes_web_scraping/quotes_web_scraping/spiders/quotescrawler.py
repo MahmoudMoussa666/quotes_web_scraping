f6f9687f91af41b3dd06d68593c399c914a28b22
MahmoudMoussa <maviad666@gmail.com>
2023-01-17 13:40:33 +0200

    finish writing of main functionality of the spider

---
+++ quotes_web_scraping/quotes_web_scraping/spiders/quotescrawler.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class QuotescrawlerSpider(scrapy.Spider):
+    name = 'quotescrawler'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        quotes = response.xpath('//div[@class="quote"]')
+        for quote in quotes:
+            yield {
+                'quote': quote.xpath('./span[@class="text"]/text()').get(),
+                'by': quote.xpath('./span/small[@class="author"]/text()').get(),
+                'tags': quote.xpath('./div[@class="tags"]/a/text()').getall()
+            }
+        next = response.xpath('//li[@class="next"]/a/@href')
+        if next:
+            yield response.follow(next.get())
